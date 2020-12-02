dfebf9e89daa3507bd9f7675ef80433ec7a2c6ec
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-02 18:57:24 -0500

    commit tree 70efafb3570a34d41578c8f92ad1121a3cc036b6

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :index
+    end
 
+    get '/new' do
+        #renders a form that can POST a name, breed, age
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        erb :display_puppy
+       #display (:name, :breed, :age)
+        
+    end
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,30 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+
+
+class Puppy
+    attr_accessor :name, :age
+    attr_reader :breed
+@@all = []
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+
+        @@all << self
+        
+    end
+
+    def new_puppy
+        Puppy.new 
+    end
+
+    # def display_puppies
+    #     Puppy.all.each do |puppy|
+    # @Puppy = puppy 
+
+    # name= puppy.name
+    # breed = puppy.breed
+    # age = puppy.age
+
+    # end
+end
